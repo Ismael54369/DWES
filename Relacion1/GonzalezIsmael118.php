6b04a0574453c3a1589ffffc1606746cf7bf6a30
IsmaTempa <ismael230102@gmail.com>
2025-09-30 18:16:01 +0200

    V1.01

---
+++ Relacion1/GonzalezIsmael118.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="./logo.png" type="image/x-icon">
+</head>
+<body>
+    <?php
+    $num1 = 20;
+    $num2 = 12;
+    $comun;
+    if ($num1 > $num2) {
+        $comun = $num1;
+    } else {
+        $comun = $num2;
+    }
+    while ($a <= 10) {
+        # code...
+    }
+    ?>
+</body>
+</html>
